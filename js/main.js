aaa9867726500acf3ade34174d7277f238f35c4e
jon Tiemann <jtiemann@digitalpersonae.com>
2013-11-12 19:57:22 -0500

    iframe fun

--- js/main.js
+++ js/main.js
@@ -124,35 +124,63 @@ function clone_simple(obj) {
 //ok, let's go...
 $(document).ready(function () {
   "use strict";
-  //init data for fruit component
-  var data = { fruits: ["banana", "strawberry", "orange"]};
+  //BEGIN PAGE INIT
+  //var data = { fruits: ["banana", "strawberry", "orange", "blueberry"]};
+  var data = eval($('fruits').attr("fruit-data"));
   //selector of container for component
-  var el = '#content';
-  //run it TODO: functionalize further for compose i.e. normalize and curry and encapsulate side-effects
+  var el = 'fruits';
+  //START FRUIT COMPONENT TODO: functionalize further for compose i.e. normalize and curry and encapsulate side-effects
   ed.uiSelector(el, viewportSize.getWidth(), ed.b_lis, data.fruits);
-  //Set up counter and element to put it in
+  //END PAGE INIT
+
+  //BEGIN   `COUNTER SETUP
   var counter = 0;
   var newdiv = document.createElement('div');
   newdiv.setAttribute('id',"counter");
   $(el).parent().append(newdiv);
   var clicker_data = {};
-  //act responsively and with different html returned
-  $( window ).on("resize", function() {
-    ed.uiSelector(el, viewportSize.getWidth(), ed.b_lis, data.fruits)
-    });
-  //show me the clickee
 
   $(el).on("click", function (evt) {
+    //add an iframe
+    if (!document.getElementById("store-iframe")){
+      var newiframe = document.createElement('iframe');
+      newiframe.setAttribute('id',"store-iframe");
+      newiframe.setAttribute('src', "http://www.brigittrueorganics.com/");
+      //newiframe.setAttribute('align', "middle");
+      //setup iframe
+      newiframe.style.width = "360px";
+      newiframe.style.height = "80%";
+      newiframe.style.marginLeft = "50px";
+      newiframe.style.position = "absolute";
+      newiframe.style.top = "50%";
+      newiframe.style.left = "50%";
+      newiframe.style.marginLeft = "-240px";
+      newiframe.style.marginTop = "-320px";
+      $(el).parent().append(newiframe);
+    }
+    else {
+      document.getElementById("store-iframe").parentNode.removeChild(document.getElementById("store-iframe"));
+    }
+
+
     //alert(evt.target.innerHTML);
     counter = counter + 1;
     //gather detailed click data
     clicker_data[evt.target.innerHTML] = clicker_data[evt.target.innerHTML] ? clicker_data[evt.target.innerHTML] + 1: 1;
+   //CHECK IF TIME TO REPORT AND IF YES, RUN REPORT
     var report_checker = ed.reporter(counter, clicker_data);
     if (report_checker !== [] && report_checker !== undefined) {
+      //FAUX REPORT
       alert(report_checker.sort());
     }
     //update total click counter
     $(el).siblings('#counter').html('<p class="fruit-counter">' + counter +  " " + ed.pluralize('like', 'likes', counter) + '</p>');
+  });
+  //END COUNTER SETUP
+
+  //RESIZE EVENT
+  $( window ).on("resize", function() {
+    ed.uiSelector(el, viewportSize.getWidth(), ed.b_lis, data.fruits)
   });
 
 });
